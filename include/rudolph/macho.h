fb0af2b61f8ba1030542a7c1d4aab39d4f2c5d85
ohnx <me@masonx.ca>
2019-01-13 19:58:22 +0000

    clean up tree and prepare for work on macho

---
+++ include/rudolph/macho.h
@@ -0,0 +1,11 @@
+#ifndef __RD_MACHO_INC_H
+#define __RD_MACHO_INC_H
+
+#ifdef RUDOLF_USE_STDLIB
+/* for uint types */
+#include <stdint.h>
+#else
+#include <rudolph/own_stdlib.h>
+#endif
+
+#endif /* __RD_MACHO_INC_H */
